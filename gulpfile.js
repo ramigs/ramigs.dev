f18772a1f6dab8512d1b28b24e9f392ec6565cc0
ramigs <ramigs@gmail.com>
2019-03-23 12:22:50 +0000

    browser-sync installed and gulp dev task created

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,44 @@
 // Load plugins
 const gulp = require("gulp");
+const server = require("./gulp-tasks/browsersync.js");
 
 // Import Tasks
 const clean = require("./gulp-tasks/clean.js");
-const eleventy = require("./gulp-tasks/eleventy.js")
+const eleventy = require("./gulp-tasks/eleventy.js");
 
-// Tasks
+// Watch files
+function watchFiles() {
+    gulp.watch(
+        [
+            "./.eleventy.js",
+            "./.eleventyignore",
+            "./src/*",
+            "./src/_data/**/*",
+            "./src/_includes/**/*",
+            "./src/_includes/**/*",
+            "./src/blogposts/**/*",
+            "./src/pages/**/*",
+            "./src/projects/**/*"
+        ],
+        eleventy.build
+    );
+}
 
+const watch = gulp.parallel(watchFiles, server.init);
+
+// Let's build
 const build = gulp.series(
     clean.dist,
     gulp.parallel(eleventy.build)
 );
 
+// Build and watch during dev
+const dev = gulp.series(
+    build,
+    watch
+);
+
+// Expose tasks to CLI
+exports.build = build;
+exports.dev = dev;
 exports.default = build;

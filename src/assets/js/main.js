167f8012f01b3996e68b179332b35037b8f7e394
ramigs <ramigs@gmail.com>
2019-11-22 20:01:14 +0000

    Prevent firing focus event when clicking on links

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -2,3 +2,12 @@ import * as nav from "./modules/navigation.js";
 nav.init();
 
 require("./vendors/prism.js");
+
+// Prevent firing focus event when clicking on links
+const links = document.querySelectorAll("a");
+
+function handleMouseDown (e) {
+	e.preventDefault();
+}
+
+links.forEach((l) => l.addEventListener("mousedown", handleMouseDown));

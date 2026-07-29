3942a868d7372b3218990af02ab8c676684f1a08
ramigs <ramigs@gmail.com>
2026-07-29 17:19:42 +0100

    added body font

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,5 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from 'astro/config';
 
 import vue from '@astrojs/vue';
 import checker from 'vite-plugin-checker';
@@ -9,6 +9,35 @@ export default defineConfig({
   site: 'https://ramigs.dev',
   trailingSlash: 'always',
   integrations: [vue()],
+  fonts: [
+    {
+      provider: fontProviders.local(),
+      name: 'IBM Plex Sans',
+      cssVariable: '--font-ibm-plex-sans',
+      fallbacks: ['sans-serif'],
+      options: {
+        variants: [
+          {
+            weight: 400,
+            style: 'normal',
+            src: ['./src/assets/fonts/ibm-plex-sans/IBMPlexSans-Regular.woff2'],
+          },
+          {
+            weight: 600,
+            style: 'normal',
+            src: [
+              './src/assets/fonts/ibm-plex-sans/IBMPlexSans-SemiBold.woff2',
+            ],
+          },
+          {
+            weight: 700,
+            style: 'normal',
+            src: ['./src/assets/fonts/ibm-plex-sans/IBMPlexSans-Bold.woff2'],
+          },
+        ],
+      },
+    },
+  ],
   vite: {
     plugins: [
       checker({
